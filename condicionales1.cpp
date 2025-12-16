914e35a6dfc18a63eeb59f11b8738ec919db4d7b
julioc74 <cesarng74@gmail.com>
2025-12-16 23:36:28 +0900

    uso de if

---
+++ condicionales1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int edad;
+    cout << "ingrese edad: " << endl;
+    cin >> edad;
+
+    // uso de if
+    if (edad >= 18){
+        cout << "Es mayor de edad";
+    }; 
+
+    return 0;
+}
